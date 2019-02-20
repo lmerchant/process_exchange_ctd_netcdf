7681e4f9c7481265bfb67f0a5383e3ecec85c33b
Lynne Merchant <lmerchant@ucsd.edu>
2019-02-20 11:41:53 -0800

    Add comments for process_exchange_ctd.py

--- process_exchange_ctd.py
+++ process_exchange_ctd.py
@@ -8,14 +8,12 @@ Read in all files and then separate out into body section and
 metadata section from the header.  The parameters and units will
 be the same for all.
 
-For netcdf, want to save all information as a variable and use N-levels
-as the index and N_profiles to keep track of each ctd set.
+For netcdf, want to save all information as an array of 
+profile number by level number where the profile number
+keeps track of each ctd set and the level number keeps track
+of each pressure level.
 
-
-xarray uses dims and coords to enable its core metadata aware operations. 
-Dimensions provide names that xarray uses instead of the axis argument found in 
-many numpy functions. Coordinates enable fast label based indexing and alignment, 
-building on the functionality of the index found on a pandas DataFrame or Series.
+Do not want N_level or N_profile set as xarray coordinates.
 
 
 Definition of xarray dataset from http://xarray.pydata.org/en/stable/data-structures.html#dataset
@@ -47,7 +45,6 @@ distinction for indexing and computations. Coordinates indicate constant/fixed/
 independent quantities, unlike the varying/measured/dependent quantities that 
 belong in data.
 
-In this program, do not want N_level or N_profile set as xarray coordinates.
 
 """
 
@@ -77,295 +74,297 @@ from get_data import get_all_data
 
 def create_folders():
 
-  # Create raw directory to contain exchange ctd csv files
-  # User needs to copy files to process into this directory
-  Config.RAW_DIR.mkdir(parents=True, exist_ok=True)
+    # Create raw directory to contain exchange ctd csv files
+    # User needs to copy files to process into this directory
+    Config.RAW_DIR.mkdir(parents=True, exist_ok=True)
 
-  # Create output directory for netcdf file
-  Config.NETCDF_DIR.mkdir(parents=True, exist_ok=True)
+    # Create output directory for netcdf file
+    Config.NETCDF_DIR.mkdir(parents=True, exist_ok=True)
 
 
 def process_folder(raw_dir):
 
-  print('Get data')
-  # Get sorted list of files in exchange ctd format to convert
-  raw_files = get_sorted_files(raw_dir, Config.SORT_ROUTINE)
+    print('Get data')
+    # Get sorted list of files in exchange ctd format to convert
+    raw_files = get_sorted_files(raw_dir, Config.SORT_ROUTINE)
 
-  # Get data from files and parse into dataframes and lists
-  metadata_all, body_all, metadata_names, parameter_names, parameter_units = get_all_data(raw_files)
-
-
-  # Get metadata and parameter data types
-  metadata_dtypes = get_metadata_dtypes(metadata_names)
-  parameter_dtypes = get_parameter_dtypes(parameter_units)
+    # Get data from files and parse into dataframes and lists
+    metadata_all, body_all, metadata_names, parameter_names, parameter_units = get_all_data(raw_files)
 
 
-  # Add all data and attributes to an xarray
-  print('Create xarrays')
+    # Get metadata and parameter data types
+    metadata_dtypes = get_metadata_dtypes(metadata_names)
+    parameter_dtypes = get_parameter_dtypes(parameter_units)
 
-  # Fill values if NaN
-  fill_value = {'flag': 9, 'datetime': np.datetime64('NaT')}
 
-  # Gather metadata as a data series
-  md_name_ds = get_metadata_data_series(metadata_all, metadata_names, metadata_dtypes, fill_value)
+    # Add all data and attributes to an xarray
+    print('Create xarrays')
 
-  # # Merge body and metadata xarrays into one
-  ctd_xr = add_body_and_metadata_to_xarray_dataset(body_all, parameter_names, parameter_dtypes, fill_value, metadata_names, md_name_ds)
+    # Fill values if NaN from combining multiple dataframes
+    fill_value = {'flag': 9, 'datetime': np.datetime64('NaT')}
 
-  # Get metadata attributes
-  metadata_attributes_file = './metadata_attributes.csv'
-  metadata_attributes = get_metadata_attributes(metadata_attributes_file)
+    # Gather metadata data frame as a data series
+    # TODO import metadata into data series initially and 
+    # not a data frame
+    metadata_ds = get_metadata_data_series(metadata_all, metadata_names, metadata_dtypes)
 
-  # Add NetCDF attributes to xarray
-  ctd_xr = add_metadata_attributes_to_xarray(metadata_attributes, metadata_names, ctd_xr)
+    # # Merge body and metadata panda dataframes and series into one xarray
+    ctd_xr = add_body_and_metadata_to_xarray_dataset(body_all, parameter_names, parameter_dtypes, fill_value, metadata_names, metadata_ds)
 
-  ctd_xr = add_parameter_attributes_to_xarray(parameter_units, ctd_xr, fill_value)
+    # Get metadata attributes
+    metadata_attributes_file = './metadata_attributes.csv'
+    metadata_attributes = get_metadata_attributes(metadata_attributes_file)
 
-  ctd_xr = add_global_attributes_to_xarray(ctd_xr)
+    # Add NetCDF attributes to xarray
+    ctd_xr = add_metadata_attributes_to_xarray(metadata_attributes, metadata_names, ctd_xr)
+
+    ctd_xr = add_parameter_attributes_to_xarray(parameter_units, ctd_xr, fill_value)
+
+    ctd_xr = add_global_attributes_to_xarray(ctd_xr)
  
-  print(ctd_xr)
+    print(ctd_xr)
 
 
-  print('Save as NetCDF')
-  # Convert xarray to NetCDF format and save
-  save_as_netcdf(ctd_xr)
+    print('Save as NetCDF')
+    # Convert xarray to NetCDF format and save
+    save_as_netcdf(ctd_xr)
 
 
-  print('Save as Mat')
-  # Convert NetCDF format to mat format and save
-  #save_as_mat(ctd_xr)
+    #print('Save as Mat')
+    # Convert NetCDF format to mat format and save
+    #save_as_mat(ctd_xr)
 
 
 def get_metadata_dtypes(metadata_names):
-  
-  metadata_dtypes = {}
+    
+    metadata_dtypes = {}
 
-  # iterate through metadata and set dtype
-  # dtype is object (string) by default
-  for name in metadata_names:
+    # iterate through metadata and set dtype
+    # dtype is object (string) by default
+    for name in metadata_names:
  
-    metadata_dtypes[name] = object
+        metadata_dtypes[name] = object
 
-    if name == 'LATITUDE':
-      metadata_dtypes[name] = np.float64
+        if name == 'LATITUDE':
+            metadata_dtypes[name] = np.float64
 
-    if name == 'LONGITUDE':
-      metadata_dtypes[name] = np.float64
+        if name == 'LONGITUDE':
+            metadata_dtypes[name] = np.float64
 
-    if name == 'DEPTH':
-      metadata_dtypes[name] = np.float64
+        if name == 'DEPTH':
+            metadata_dtypes[name] = np.float64
 
-    if name == 'DATETIME':
-      metadata_dtypes[name] = np.datetime64
+        if name == 'DATETIME':
+            metadata_dtypes[name] = np.datetime64
 
-    if name == 'SECS_FROM_1970':
-      metadata_dtypes[name] = np.float64      
+        if name == 'SECS_FROM_1970':
+            metadata_dtypes[name] = np.float64      
 
 
-  return metadata_dtypes
+    return metadata_dtypes
 
 
 def get_parameter_dtypes(parameter_units):
-  
-  parameter_dtypes = {}
+    
+    parameter_dtypes = {}
 
-  # iterate through parameters and set dtype
-  for key, value in parameter_units.items():
+    # iterate through parameters and set dtype
+    for key, value in parameter_units.items():
 
-    if 'FLAG' in key:
-      parameter_dtypes[key] = np.int8
+        if 'FLAG' in key:
+            parameter_dtypes[key] = np.int8
 
-    else:
-      parameter_dtypes[key] = np.float64
-
-
-  return parameter_dtypes
+        else:
+            parameter_dtypes[key] = np.float64
 
 
-def get_metadata_data_series(metadata_all, metadata_names, metadata_dtypes, fill_value):
-
-  # metadata_all is a list of data frames
-  # concat all data frames into one
-
-  md_name_ds = {}
-
-  df = pd.concat(metadata_all)
-
-  df = df.reset_index(drop=True)
-
-  for md_name in metadata_names:
-
-    series = df[md_name].squeeze()
-
-    md_name_dtype = metadata_dtypes[md_name]
-
-    md_name_ds[md_name] = series.astype(md_name_dtype)
-
-  return md_name_ds
+    return parameter_dtypes
 
 
-def add_body_and_metadata_to_xarray_dataset(body_all, parameter_names, parameter_dtypes, fill_value, metadata_names, md_name_ds):
+def get_metadata_data_series(metadata_all, metadata_names, metadata_dtypes):
 
-  # index column of each body dataframe was renamed 'N_level'
-  # The xarray dimension N_profile keeps track of each dataframe
+    # metadata_all is a list of data frames
+    # concat all data frames into one series
 
-  # Create xarray dataset from list of dataframes with
-  # Dimension order (N_level, N_profile)
-  ctd_xr = xr.concat([df.to_xarray() for df in body_all], dim = 'index')
+    metadata_ds = {}
+
+    df = pd.concat(metadata_all)
+
+    # Reset index from combining all data frames
+    df = df.reset_index(drop=True)
+
+    for md_name in metadata_names:
+
+        series = df[md_name].squeeze()
+
+        md_name_dtype = metadata_dtypes[md_name]
+
+        metadata_ds[md_name] = series.astype(md_name_dtype)
+
+    return metadata_ds
+
+
+def add_body_and_metadata_to_xarray_dataset(body_all, parameter_names, parameter_dtypes, fill_value, metadata_names, metadata_ds):
+
+    # index column of each body dataframe was renamed 'N_level'
+    # The xarray dimension N_profile keeps track of each dataframe
+
+    # Create xarray dataset from list of dataframes with
+    # Dimension order (N_level, N_profile)
+    ctd_xr = xr.concat([df.to_xarray() for df in body_all], dim = 'index')
  
-  # Convert to dataframe and assign data types
-  ctd_pd = ctd_xr.to_dataframe()
+    # Convert to dataframe and assign data types
+    ctd_pd = ctd_xr.to_dataframe()
 
-  # Fill NaN values in qc flags with an interger fill value
-  for name in parameter_names:
-    if 'FLAG' in name and parameter_dtypes[name] == np.int8:
-      ctd_pd.loc[ctd_pd[name].isnull(), [name]] = fill_value['flag']
+    # Fill NaN values in qc flags with an interger fill value
+    for name in parameter_names:
+        if 'FLAG' in name and parameter_dtypes[name] == np.int8:
+            ctd_pd.loc[ctd_pd[name].isnull(), [name]] = fill_value['flag']
 
-  # Assign data types
-  ctd_pd = ctd_pd.astype(parameter_dtypes)
+    # Assign data types
+    ctd_pd = ctd_pd.astype(parameter_dtypes)
 
-  # Convert back to xarray
-  ctd_xr = ctd_pd.to_xarray()
+    # Convert back to xarray
+    ctd_xr = ctd_pd.to_xarray()
 
-  # Transpose dimension order to (N_profile, N_level)
-  ctd_xr = ctd_xr.transpose()
-
-
-  # Since merge appended to top, reverse order of metadata names
-  metadata_names_reversed = metadata_names[::-1]
-
-  for md_name in metadata_names_reversed:
-
-    name_xr = md_name_ds[md_name].to_xarray()
-
-    ctd_xr = xr.merge([name_xr, ctd_xr])
-
-  # Rename index
-  ctd_xr =  ctd_xr.rename({'index':'N_profile'})
+    # Transpose dimension order to (N_profile, N_level)
+    ctd_xr = ctd_xr.transpose()
 
 
-  return ctd_xr
+    # Since merge appended to top, reverse order of metadata names
+    metadata_names_reversed = metadata_names[::-1]
+
+    for md_name in metadata_names_reversed:
+
+        name_xr = metadata_ds[md_name].to_xarray()
+
+        ctd_xr = xr.merge([name_xr, ctd_xr])
+
+    # Rename index
+    ctd_xr =  ctd_xr.rename({'index':'N_profile'})
+
+
+    return ctd_xr
 
 
 def get_metadata_attributes(attribute_file):
-  
-  data = []
+    
+    data = []
 
-  with open(attribute_file) as f:
-    for row in csv.DictReader(f):
-        data.append(row)
+    with open(attribute_file) as f:
+        for row in csv.DictReader(f):
+                data.append(row)
 
-  json_string = json.dumps(data)
+    json_string = json.dumps(data)
 
-  json_data = json.loads(json_string)
+    json_data = json.loads(json_string)
 
-  return json_data
+    return json_data
 
 
 def add_metadata_attributes_to_xarray(attributes, metadata_names, ctd_xr):
 
-  for attribute in attributes:
+    for attribute in attributes:
 
-    if attribute['variable'] in metadata_names:
-      ctd_xr[attribute['variable']].attrs = {'units': attribute['units'], 'long_name': attribute['long_name']}
+        if attribute['variable'] in metadata_names:
+            ctd_xr[attribute['variable']].attrs = {'units': attribute['units'], 'long_name': attribute['long_name']}
 
-  return ctd_xr
+    return ctd_xr
 
 
 def add_parameter_attributes_to_xarray(parameter_units, ctd_xr, fill_value):
 
-  for name in parameter_units:
+    for name in parameter_units:
 
-    if 'FLAG' in name:
-      ctd_xr[name].attrs = {'_FillValue': fill_value['flag']} 
-    else:
-      ctd_xr[name].attrs = {'units': parameter_units[name]} 
+        if 'FLAG' in name:
+            ctd_xr[name].attrs = {'_FillValue': fill_value['flag']} 
+        else:
+            ctd_xr[name].attrs = {'units': parameter_units[name]} 
 
-  return ctd_xr
+    return ctd_xr
 
 
 def add_global_attributes_to_xarray(ctd_xr):
 
-  ctd_xr.attrs['title'] = 'CTD data'
+    ctd_xr.attrs['title'] = 'CTD data'
 
-  return ctd_xr
+    return ctd_xr
 
 
 def save_as_netcdf(ctd_xr):
 
-  # Save xarray as netcdf
+    # Save xarray as netcdf
 
-  # Get expocode to include in filename
-  expocode = str(ctd_xr['EXPOCODE'][0].values)
+    # Get expocode to include in filename
+    expocode = str(ctd_xr['EXPOCODE'][0].values)
 
-  filename = expocode + '.nc'
+    filename = expocode + '.nc'
 
-  netcdf_filename = Config.NETCDF_DIR.joinpath(filename)
+    netcdf_filename = Config.NETCDF_DIR.joinpath(filename)
 
-  try:
-    os.remove(netcdf_filename)
-  except:
-    pass
+    try:
+        os.remove(netcdf_filename)
+    except:
+        pass
 
-  ctd_xr.drop(['N_profile', 'N_level'])
+    # Drop variables or index labels from this dataset.
+    ctd_xr.drop(['N_profile', 'N_level'])
 
-  ctd_xr.to_netcdf(netcdf_filename)
-
-
-
-
-def save_as_mat(ctd_xr):
-
-  # Get expocode to include in filename
-  expocode = ctd_xr['EXPOCODE'][0].values[0]
-
-  filename = expocode + '.mat'
-
-  mat_filename = Config.MAT_DIR.joinpath(filename)
-
-  try:
-    os.remove(mat_filename)
-  except:
-    pass
-
-
-  ctd_dict = ctd_xr.to_dict()
-
-  #print(ctd_dict['data_vars']['CTDFLUOR'])
-
-  # {'data': [[values]], 'dims': ('N_profile', 'N_level'), 'attrs': {'units': 'MG/M^3'}}
-
-  ctd_fluor = ctd_dict['data_vars']['CTDFLUOR']
-
-  ctd_fluor_data = ctd_fluor['data']
-
-  N_profile = len(ctd_fluor_data)
-  N_level = len(ctd_fluor_data[0])
-
-  #print(N_profile, N_level)
-
-  ctd_fluor_units = ctd_fluor['attrs']['units']
-
-  #print(ctd_fluor_units)
+    ctd_xr.to_netcdf(netcdf_filename)
 
 
 
-  sio.savemat( mat_filename, dict( [ ('CTDFLUOR', ctd_fluor_data), ('units', ctd_fluor_units) ] ) ) 
+
+# def save_as_mat(ctd_xr):
+
+#     # Get expocode to include in filename
+#     expocode = ctd_xr['EXPOCODE'][0].values[0]
+
+#     filename = expocode + '.mat'
+
+#     mat_filename = Config.MAT_DIR.joinpath(filename)
+
+#     try:
+#         os.remove(mat_filename)
+#     except:
+#         pass
 
 
+#     ctd_dict = ctd_xr.to_dict()
+
+#     #print(ctd_dict['data_vars']['CTDFLUOR'])
+
+#     # {'data': [[values]], 'dims': ('N_profile', 'N_level'), 'attrs': {'units': 'MG/M^3'}}
+
+#     ctd_fluor = ctd_dict['data_vars']['CTDFLUOR']
+
+#     ctd_fluor_data = ctd_fluor['data']
+
+#     N_profile = len(ctd_fluor_data)
+#     N_level = len(ctd_fluor_data[0])
+
+#     #print(N_profile, N_level)
+
+#     ctd_fluor_units = ctd_fluor['attrs']['units']
+
+#     #print(ctd_fluor_units)
+
+
+
+#     sio.savemat( mat_filename, dict( [ ('CTDFLUOR', ctd_fluor_data), ('units', ctd_fluor_units) ] ) ) 
 
 
 
 
 def main():
 
-  create_folders()
+    create_folders()
 
-  process_folder(Config.RAW_DIR)
+    process_folder(Config.RAW_DIR)
 
-  
+    
 
 
 
 if __name__ == '__main__':
-  main()
+    main()
